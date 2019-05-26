54b4806221d72bcd3a4249ed0f38152f295a3bdf
Laz <lazjramos@gmail.com>
2019-05-25 22:36:54 -0500

    rendering task titles

--- gql-client/src/App.js
+++ gql-client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-function App() {
+function App (props) {
+  let task = props.data.task
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {
+          props.data.loading
+            ? "loading"
+            : props.data.tasks.map(task => (<li>{task.title}</li>))
+        }
+      </ul>
     </div>
   );
 }
 
-export default App;
+const queryAllTasks = gql`
+  query {
+    tasks {
+      id
+      title
+      description
+    }
+  }
+`;
+
+const appWithData = graphql(queryAllTasks)(App);
+
+export default appWithData;
